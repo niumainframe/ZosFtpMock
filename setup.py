d71f87122ce9bc8c1170854c38ec88129eb358ad
Vincent Schramer <vinciple@gmail.com>
2014-02-16 12:12:15 -0600

    Fixed version so that it shouldn't download from pip repo.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+pyftpdlib_commitver = 'b7f5cd8'
+
 setup(
     name='ZosFtpMock',
     version='0.1.1',
@@ -10,16 +12,16 @@ setup(
     packages=['ZosFtpMock'],
     scripts=['bin/zosftpd.py'],
     
-    description='Roughly simulates how the FTP server on the zOS mainframe behaves for job submissions.
+    description='Roughly simulates how the FTP server on the zOS mainframe behaves for job submissions.',
     
     long_description=open('README.md').read(),
     
     install_requires=[
-        "pyftpdlib >= 1.3.0",
+        "pyftpdlib == 1.3.0-scvncfork",
         "argparse >= 1.2.1"
     ],
     
     dependency_links = [
-        "https://github.com/scvnc/pyftpdlib-mod/tarball/49a576c#egg=pyftpdlib-1.3.0"
+        "https://github.com/scvnc/pyftpdlib-mod/tarball/" + pyftpdlib_commitver + "#egg=pyftpdlib-1.3.0-scvncfork"
     ]
 )
